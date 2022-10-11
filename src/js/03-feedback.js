f1ccca5decbafdb3e6e8fdfcf5cc53aed4d70e7d
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-10-12 01:05:14 +0300

    add autofield function

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,25 @@
 import throttle from 'lodash.throttle';
 const inputRef = document.querySelector('.feedback-form');
 
-inputRef.addEventListener('input', onFormInput);
+const ageRef = document.querySelector('.js-age');
+
+inputRef.addEventListener('input', throttle(onFormInput), 500);
 inputRef.addEventListener('submit', onSubmitForm);
 
 const USER_TEMP_DATA = 'feedback-form-state';
 
 let userData = {};
+let formElements = Array.from(inputRef.elements);
+ageRef.textContent = 'Choose your age';
 
 function onFormInput(evt) {
   userData[evt.target.name] = evt.target.value;
+
   const userDataStringify = JSON.stringify(userData);
   localStorage.setItem(USER_TEMP_DATA, userDataStringify);
+  if (userData.age) {
+    ageRef.textContent = `${userData.age} years old`;
+  }
 }
 
 userSavedDataOutput();
@@ -21,19 +29,36 @@ function userSavedDataOutput() {
 
   if (savedUserData) {
     userData = savedUserData;
-    inputRef.email.value = savedUserData.email;
-    inputRef.message.value = savedUserData.message;
+
+    const userDataKey = Object.keys(userData);
+    userDataKey.map(key => {
+      formElements.map(element => {
+        if (element.name === key) {
+          element.value = userData[key];
+        }
+      });
+    });
+    if (userData.age) {
+      ageRef.textContent = `${userData.age} years old`;
+    }
   }
 }
 
 function onSubmitForm(evt) {
-  if (!inputRef.email.value || !inputRef.message.value) {
-    alert('Please, enter data in all fields');
-    return;
-  }
   evt.preventDefault();
 
+  for (const el of formElements) {
+    if (el.name && !el.value) {
+      alert('Please, enter data in all fields');
+
+      return;
+    }
+  }
   evt.currentTarget.reset();
+  ageRef.textContent = 'Thanks';
+  setTimeout(() => {
+    ageRef.textContent = 'Choose your age';
+  }, 1000);
 
   console.log(userData);
   userData = {};
