02fd2cdd062b7c07b58bc63026baac890aa65682
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-10-27 17:17:10 +0300

    add fit ignore files

--- src/fetch-country-api.js
+++ src/fetch-country-api.js
@@ -2,6 +2,6 @@ const BASE_URL = 'https://restcountries.com/v3.1/';
 
 export default function fetchCountries(countryName) {
   return fetch(
-    `${BASE_URL}name/${countryName}?fields=name,capital,population,flags,languages `
+    `${BASE_URL}name/${countryName}?fields=name,capital,population,flags,languages,maps`
   ).then(r => r.json());
 }
--- src/load-spinner.js
+++ src/load-spinner.js
@@ -4,17 +4,17 @@ const opts = {
   top: '25%', // Top position relative to parent
   left: '180px', // Left position relative to parent
 
-  lines: 14, // The number of lines to draw
-  length: 26, // The length of each line
+  lines: 16, // The number of lines to draw
+  length: 27, // The length of each line
   width: 10, // The line thickness
-  radius: 33, // The radius of the inner circle
-  scale: 0.35, // Scales overall size of the spinner
+  radius: 29, // The radius of the inner circle
+  scale: 0.5, // Scales overall size of the spinner
   corners: 1, // Corner roundness (0..1)
-  speed: 1.5, // Rounds per second
+  speed: 1.2, // Rounds per second
   rotate: 0, // The rotation offset
-  animation: 'spinner-line-fade-more', // The CSS animation name for the lines
+  animation: 'spinner-line-shrink', // The CSS animation name for the lines
   direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#61668f', // CSS color or array of colors
+  color: '#5a69dd', // CSS color or array of colors
   fadeColor: '', // CSS color or array of colors
 
   shadow: 'box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;', // Box-shadow for the lines
