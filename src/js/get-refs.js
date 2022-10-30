b9768313ad7738e89528890bd0cd5e416211ffd0
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-10-30 07:37:34 +0300

    fix getRefs

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -1,1 +1,6 @@
-export default getRefs = selector => document.querySelector(selector);
+const getRefs = selector => document.querySelector(selector);
+
+// export default function getRefs(selector) {
+//   return document.querySelector(selector);
+// }
+export default getRefs;
